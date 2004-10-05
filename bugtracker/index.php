c02dc3a84f86ac512ea840398019f414fe0f5cb8
Adam Harvey <adam@adamharvey.name>
2004-10-05 11:01:18 +0000

    Added magic_quotes_gpc workaround.

--- bugtracker/index.php
+++ bugtracker/index.php
@@ -8,8 +8,20 @@
  * Known bugs: None, but there almost certainly are some.
  */
 
+if (get_magic_quotes_gpc()) {
+	function stripslashes_deep($value) {
+		$value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
+
+		return $value;
+	}
+
+	$_POST = array_map('stripslashes_deep', $_POST);
+	$_GET = array_map('stripslashes_deep', $_GET);
+	$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+}
+
 define('CODENAME', 'Roach Motel');
-define('VERSION', '3.2.0');
+define('VERSION', '3.2.1');
 
 ob_start();
 
