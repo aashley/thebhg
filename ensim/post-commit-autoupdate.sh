b9fd6323d6b323c52a3ca61b86c8510fffbf2eb5
Adam Ashley <aashley@adamashley.name>
2004-10-10 12:08:25 +0000

    Updated autoupdate scripts

---
+++ ensim/post-commit-autoupdate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+REPOS="$1"
+REV="$2"
+
+function LogMsg() {
+  local DATEFORMAT
+
+  if [ -z "$*" ]; then
+    echo
+  else
+    DATEFORMAT="%d %b %Y %H:%M:%S %Z"
+    echo -- [`date "+$DATEFORMAT"`]: $*
+  fi
+  return 0
+}
+
+function checkUpdate() {
+  local UPDATE dir=$1 site=$2
+
+  LogMsg "Checking Revision $REV in $REPOS for $dir update"
+  /usr/local/bin/svnlook changed "$REPOS" -r "$REV" | grep $dir > /dev/null
+  UPDATE=$?
+
+  if [[ $UPDATE -eq 0 ]]; then
+		LogMsg "Update found for $dir"
+    sudo /usr/local/bin/update-site $site 2>&1
+	else
+		LogMsg "No Update Found for $dir"
+  fi
+}
+
+# Switch to /tmp so that svnlook can create its tmp files without problems
+cd /tmp
+
+LogMsg "New Commit: $REV"
+
+checkUpdate projects/trunk/holonet holonet-devel
+
+LogMsg "Revision $REV complete"
