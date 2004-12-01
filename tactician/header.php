4f1395809925d8ccb70c2eaf281a0f060fab6439
Adam Harvey <adam@adamharvey.name>
2004-12-01 16:37:09 +0000

    Fixed roster_link() from subdirectories.

--- tactician/header.php
+++ tactician/header.php
@@ -109,7 +109,7 @@ function roster_link($id, $name = '') {
 		$id = $roster->GetPerson($id);
 	}
 
-	return '<A HREF="hunter.php?id=' . $id->GetID() . '" TITLE="' . htmlspecialchars($id->GetName()) . '">' . htmlspecialchars($name == '' ? $id->GetName() : $name) . '</A>';
+	return '<A HREF="/hunter.php?id=' . $id->GetID() . '" TITLE="' . htmlspecialchars($id->GetName()) . '">' . htmlspecialchars($name == '' ? $id->GetName() : $name) . '</A>';
 }
 
 function hunter_dropdown($exclude = array(0, 16), $select = false) {
