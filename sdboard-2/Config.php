beeaf5bb250663a79e8c1f3f653ae909f4e8e694
Adam Harvey <adam@adamharvey.name>
2004-12-31 17:41:57 +0000

    Removed old SDBoard 2 tree.

--- sdboard-2/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SDBOARD_ADMIN', '94,666');
-define('SDBOARD_DSN', 'mysql://thebhg_boards:rbh1tmtd4@localhost/thebhg_boards');
-define('SDBOARD_TITLE', 'Bounty Hunter\'s Guild Message Board');
-define('SDBOARD_USERTYPE', 'BHG');
-?>
