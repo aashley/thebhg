9658c75f59aaad787ceaff62a0dbe4a2e95b9cac
Adam Ashley <aashley@adamashley.name>
2005-01-15 17:01:41 +0000

    more IE de-fucking

--- holonet/hr.php
+++ holonet/hr.php
@@ -1,5 +1,6 @@
 <?php
 function hr() {
-	echo '<table border=0 width="100%"><tr><td style="border-bottom: solid 1px black">&nbsp;</td></tr></table><br>';
+//	echo '<table border=0 width="100%"><tr><td style="border-bottom: solid 1px black">&nbsp;</td></tr></table><br>';
+	echo '<hr/>';
 }
 ?>
