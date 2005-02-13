0a7f1004714433b0058c2e479b89edbea96822e3
Adam Ashley <aashley@adamashley.name>
2005-02-13 02:34:54 +0000

    syncing ka site to production

--- ka/kabals/index.php
+++ ka/kabals/index.php
@@ -8,7 +8,7 @@
 				<h2><?=$kabal->GetName()?> Kabal</h2>
 			<table cellpadding="0" cellspacing="0" class="invisible">
 				<tr>
-					<td colspan="2">Chief: <b><?=$chief->GetName()?></b> - <b><a shref="mailto:<?=$chief->GetEMail()?>"><?=$chief->GetEMail()?></a></b></td> <td colSpan="4" rowspan="6" width="82" ><img src="<?=$kabal->getlogourl()?>"></td>
+					<td colspan="2">Chief: <b><?=$chief->GetName()?></b> - <b><a href="mailto:<?=$chief->GetEMail()?>"><?=$chief->GetEMail()?></a></b></td> <td colSpan="4" rowspan="6" width="82" ><img src="<?=$kabal->getlogourl()?>"></td>
 				</tr>
 				<tr>
 					<td colspan="2">CRA: <b><?=$cra->GetName()?></b> - <b><a href="mailto:<?=$cra->GetEMail()?>"><?=$cra->GetEMail()?></a></b></td>
