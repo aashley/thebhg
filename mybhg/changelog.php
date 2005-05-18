a7db1d315fdf343851226b5b933b00fe481483ad
Adam Harvey <adam@adamharvey.name>
2005-05-18 12:13:30 +0000

    Fixed changelog display.

--- mybhg/changelog.php
+++ mybhg/changelog.php
@@ -4,7 +4,7 @@ include('header.php');
 
 $log = preg_replace(array('/^-- /m', '/ --$/m', '/^[0-9]+. (.*)$/m', '/^$/m'),
 		    array('<b>', '</b><ol>', '<li>\1</li>', '</ol>'),
-		    file_get_contents('changelog.txt'));
+		    htmlspecialchars(file_get_contents('changelog.txt')));
 
 $table = new Table();
 $table->AddRow($log);
