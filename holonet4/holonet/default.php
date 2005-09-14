3ffcdbd62986a4731ff3bfac6216e9d18aae0920
Adam Ashley <aashley@adamashley.name>
2005-09-14 12:50:08 +0000

    try the roster

--- holonet4/holonet/default.php
+++ holonet4/holonet/default.php
@@ -8,6 +8,10 @@ class page_holonet_default extends holonet_page {
 
 		$this->addBodyContent('<p>Hello World</p>');
 
+		$person = bhg_roster::getPerson(94);
+
+		$this->addBodyContent('<p>'.$person->getIDLine().'</p>');
+
 		$menu = new holonet_menu();
 		$menu->title = 'News';
 		$menu->addItem(new holonet_menu_item('News', '/holonet/'));
