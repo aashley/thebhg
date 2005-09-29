c6bfe342a4d5dc2f8ceffd974fafdeaca6a96d95
Adam Ashley <aashley@adamashley.name>
2005-09-29 07:20:27 +0000

    oops

--- roster4/database/convert.sql
+++ roster4/database/convert.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 -- Code IDs
 ALTER TABLE coders RENAME core_code;
-ALTER TABLE core_code DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE core_code DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE core_code CHANGE md5 hash CHAR(32) NOT NULL;
 UPDATE core_code SET hash = LOWER(hash);
 ALTER TABLE core_code ADD UNIQUE KEY(hash);
@@ -11,7 +11,7 @@ ALTER TABLE core_code ADD UNIQUE KEY(hash);
 DROP TABLE `cs_bonus_points`, `cs_classes`, `cs_field_options`, `cs_fields`, `cs_pending_fields`, `cs_sheet_fields`, `cs_sheets`, `cs_unused_xp`, `cs_used_xp`, `new_members`;
 
 -- Roster Bio Data
-ALTER TABLE `roster_biographical_data` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_biographical_data` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_biographical_data ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_biographical_data ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE roster_biographical_data ADD COLUMN datedeleted DATETIME;
@@ -23,7 +23,7 @@ DROP TABLE roster_blacklist;
 
 -- Roster Cadres
 ALTER TABLE roster_cadres RENAME roster_cadre;
-ALTER TABLE `roster_cadre` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_cadre` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_cadre ADD COLUMN datecreated DATETIME NOT NULL;
 UPDATE roster_cadre SET datecreated = FROM_UNIXTIME(date_created);
 ALTER TABLE roster_cadre DROP COLUMN date_created;
@@ -38,7 +38,7 @@ ALTER TABLE roster_cadre DROP COLUMN date_deleted;
 
 -- Roster Division Categories
 ALTER TABLE roster_division_categories RENAME roster_division_category;
-ALTER TABLE `roster_division_category` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_division_category` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_division_category ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_division_category ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE roster_division_category ADD COLUMN datedeleted DATETIME;
@@ -48,7 +48,7 @@ ALTER TABLE roster_division_category CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Roster Divisions
 ALTER TABLE roster_divisions RENAME roster_division;
-ALTER TABLE `roster_division` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_division` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_division ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_division ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE roster_division ADD COLUMN datedeleted DATETIME;
@@ -58,7 +58,7 @@ ALTER TABLE roster_division CHANGE home_page_url homepageurl VARCHAR(200);
 
 -- Roster History
 ALTER TABLE roster_history RENAME history_event;
-ALTER TABLE `history_event` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `history_event` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE history_event ADD COLUMN datecreated DATETIME NOT NULL;
 UPDATE history_event SET datecreated = FROM_UNIXTIME(`date`);
 ALTER TABLE history_event DROP COLUMN `date`;
@@ -66,12 +66,12 @@ ALTER TABLE history_event ADD COLUMN item4 VARCHAR(250);
 
 -- Roster New Member
 ALTER TABLE roster_new_members RENAME roster_new_person;
-ALTER TABLE `roster_new_person` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_new_person` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_new_person ADD COLUMN datecreated DATETIME NOT NULL;
 UPDATE roster_new_person SET datecreated = NOW();
 
 -- Roster Position
-ALTER TABLE `roster_position` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_position` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_position ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_position ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE roster_position ADD COLUMN datedeleted DATETIME;
@@ -82,7 +82,7 @@ ALTER TABLE roster_position CHANGE istrainee trainee INT(1) NOT NULL;
 ALTER TABLE roster_position CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Roster Rank
-ALTER TABLE `roster_rank` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_rank` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_rank ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_rank ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE roster_rank ADD COLUMN datedeleted DATETIME;
@@ -95,7 +95,7 @@ ALTER TABLE roster_rank CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Roster Person
 ALTER TABLE roster_roster RENAME roster_person;
-ALTER TABLE `roster_person` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `roster_person` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_person ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE roster_person ADD COLUMN datedeleted DATETIME;
 UPDATE roster_person SET dateupdated = FROM_UNIXTIME(last_updated);
@@ -113,7 +113,7 @@ ALTER TABLE roster_person CHANGE hasship ship INT(1) NOT NULL;
 
 -- MedalBoard Awarded Medals
 ALTER TABLE mb_awarded_medals RENAME medalboard_award;
-ALTER TABLE `medalboard_award` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `medalboard_award` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_award ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE medalboard_award ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE medalboard_award CHANGE recipientid recipient INT(11) NOT NULL;
@@ -121,7 +121,7 @@ ALTER TABLE medalboard_award CHANGE awarderid awarder INT(11) NOT NULL;
 
 -- MedalBoard Medal Categories
 ALTER TABLE mb_medal_categories RENAME medalboard_category;
-ALTER TABLE `medalboard_category` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `medalboard_category` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_category ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE medalboard_category ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE medalboard_category ADD COLUMN datedeleted DATETIME;
@@ -129,7 +129,7 @@ ALTER TABLE medalboard_category CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- MedalBoard Medal Groups
 ALTER TABLE mb_medal_groups RENAME medalboard_group;
-ALTER TABLE `medalboard_group` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `medalboard_group` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_group ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE medalboard_group ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE medalboard_group ADD COLUMN datedeleted DATETIME;
@@ -140,7 +140,7 @@ DROP TABLE mb_medal_descriptions;
 
 -- MedalBoard Medal Names
 ALTER TABLE mb_medal_names RENAME medalboard_medal;
-ALTER TABLE `medalboard_medal` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `medalboard_medal` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_medal ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE medalboard_medal ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE medalboard_medal ADD COLUMN datedeleted DATETIME;
@@ -148,7 +148,7 @@ ALTER TABLE medalboard_medal CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Library Books
 ALTER TABLE library_books RENAME library_book;
-ALTER TABLE `library_book` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `library_book` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_book ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE library_book ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE library_book ADD COLUMN datedeleted DATETIME;
@@ -156,7 +156,7 @@ ALTER TABLE library_book CHANGE image_type imagetype TEXT;
 
 -- Library Chapters
 ALTER TABLE library_chapters RENAME library_chapter;
-ALTER TABLE `library_chapter` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `library_chapter` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_chapter ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE library_chapter ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE library_chapter ADD COLUMN datedeleted DATETIME;
@@ -164,14 +164,14 @@ ALTER TABLE library_chapter CHANGE sort_order sortorder INT(11) NOT NULL;
 
 -- Library Moderators
 ALTER TABLE library_moderators RENAME library_moderator;
-ALTER TABLE `library_moderator` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `library_moderator` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_moderator ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE library_moderator ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE library_moderator ADD COLUMN datedeleted DATETIME;
 
 -- Library Sections
 ALTER TABLE library_sections RENAME library_section;
-ALTER TABLE `library_section` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `library_section` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_section ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE library_section ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE library_section ADD COLUMN datedeleted DATETIME;
@@ -180,7 +180,7 @@ ALTER TABLE library_section CHANGE useHTML html INT(1) NOT NULL;
 
 -- Library Shelves
 ALTER TABLE library_shelves RENAME library_shelf;
-ALTER TABLE `library_shelf` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `library_shelf` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_shelf ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE library_shelf ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE library_shelf ADD COLUMN datedeleted DATETIME;
@@ -188,7 +188,7 @@ ALTER TABLE library_shelf CHANGE sort_order sortorder INT(11) NOT NULL;
 
 -- Newsboard
 ALTER TABLE newsboard RENAME news_item;
-ALTER TABLE `news_item` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `news_item` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE news_item ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE news_item ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE news_item ADD COLUMN datedeleted DATETIME;
@@ -213,7 +213,7 @@ INSERT INTO `college_reward` (datecreated, dateupdated, exam, rewardtype, requir
 
 -- NTC Exams
 ALTER TABLE ntc_exams RENAME college_exam;
-ALTER TABLE `college_exam` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `college_exam` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_exam ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE college_exam ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_exam CHANGE date_deleted datedeleted DATETIME;
@@ -224,21 +224,21 @@ ALTER TABLE college_exam DROP COLUMN credit_award;
 
 -- NTC Questions
 ALTER TABLE ntc_exam_questions RENAME college_exam_question;
-ALTER TABLE `college_exam_question` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `college_exam_question` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_exam_question ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE college_exam_question ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_exam_question CHANGE date_deleted datedeleted DATETIME;
 
 -- NTC Exam Markers
 ALTER TABLE ntc_exam_markers RENAME college_exam_marker;
-ALTER TABLE `college_exam_marker` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `college_exam_marker` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_exam_marker ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE college_exam_marker ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_exam_marker ADD COLUMN datedeleted DATETIME;
 
 -- NTC Exam Completed
 ALTER TABLE ntc_exam_completed RENAME college_submission;
-ALTER TABLE `college_submission` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `college_submission` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_submission CHANGE date_taken datecreated DATETIME NOT NULL;
 ALTER TABLE college_submission ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_submission CHANGE bhg_id submitter INT(11) NOT NULL;
@@ -247,7 +247,7 @@ ALTER TABLE college_submission CHANGE has_passed passed INT(1) NOT NULL;
 
 -- NTC Exam Answers
 ALTER TABLE ntc_exam_answers RENAME college_submission_answer;
-ALTER TABLE `college_submission_answer` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE `college_submission_answer` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_submission_answer ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE college_submission_answer ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_submission_answer CHANGE result submission INT(11) NOT NULL;
