388d17044770c41eaaaa984afaeddbd335c40038
Adam Ashley <aashley@adamashley.name>
2005-10-27 06:41:18 +0000

    make why column not null

--- roster4/database/convert.sql
+++ roster4/database/convert.sql
@@ -130,6 +130,7 @@ ALTER TABLE medalboard_award CHANGE recipientid recipient INT(11) NOT NULL;
 ALTER TABLE medalboard_award CHANGE awarderid awarder INT(11) NOT NULL;
 UPDATE medalboard_award SET datecreated = FROM_UNIXTIME(`date`), dateupdated = FROM_UNIXTIME(`date`);
 ALTER TABLE medalboard_award DROP COLUMN `date`;
+ALTER TABLE `medalboard_award` CHANGE `why` `why` TEXT NOT NULL;
 
 -- MedalBoard Medal Categories
 ALTER TABLE mb_medal_categories RENAME medalboard_category;
