e2f7ef3c30021cdfef642d13d0a27b4f8cfd4292
Adam Harvey <adam@adamharvey.name>
2005-11-03 04:17:26 +0000

    Added lyarna-devel.

--- ensim/post-commit-autoupdate.sh
+++ ensim/post-commit-autoupdate.sh
@@ -39,8 +39,9 @@ LogMsg "New Commit: $REV"
 #checkUpdate users/gravant/trunk/workspace gravant-workspace
 checkUpdate projects/trunk/holonet holonet3-devel
 checkUpdate projects/trunk/holonet4 holonet4-devel
-checkUpdate projects/trunk/mybhg mybhg-devel
 checkUpdate projects/trunk/ka ka-devel
+checkUpdate projects/trunk/lyarna lyarna-devel
+checkUpdate projects/trunk/mybhg mybhg-devel
 checkUpdate projects/trunk/roster4 roster4
 checkUpdate projects/trunk/scum scum-devel
 checkUpdate projects/trunk/tactician tactician-devel
