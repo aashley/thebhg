3af1bcf837fc32dd989e0c6465a1c0917bf108e0
Adam Ashley <aashley@adamashley.name>
2005-12-17 15:50:30 +0000

    sort list of sites alphabetically

--- lyarna-hosting/index.php
+++ lyarna-hosting/index.php
@@ -17,6 +17,8 @@ foreach ($dir as $file) {
 
 }
 
+$sites = array_sort($sites);
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
