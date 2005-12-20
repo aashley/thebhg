da88a7fcbb4b6ea5775141dace7571c4e7169489
Adam Ashley <aashley@adamashley.name>
2005-12-20 13:10:47 +0000

    adding google adsense

--- scum/template/footer.php
+++ scum/template/footer.php
@@ -1,4 +1,20 @@
-</td></tr></table></td></tr></table>
+</td></tr></table></td></tr>
+<tr><td id="google"><script type="text/javascript"><!--
+google_ad_client = "pub-4673636750726986";
+google_ad_width = 728;
+google_ad_height = 90;
+google_ad_format = "728x90_as";
+google_ad_type = "text";
+google_ad_channel ="";
+google_color_border = "333333";
+google_color_bg = "000000";
+google_color_link = "FFFFFF";
+google_color_url = "999999";
+google_color_text = "CCCCCC";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+	</script></td></tr></table>
 
 </body>
 </html>
