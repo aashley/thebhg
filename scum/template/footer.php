bca29dd57c103d5d1579f93e6a3cf647425bf680
Adam Ashley <aashley@adamashley.name>
2005-12-21 13:59:01 +0000

    replaced google code with own banner system

--- scum/template/footer.php
+++ scum/template/footer.php
@@ -1,21 +1,23 @@
 </td></tr></table></td></tr>
 <tr><td id="bottom" colspan="2"></td></tr>
-<tr><td class="main"></td><td id="google"><script type="text/javascript"><!--
-google_ad_client = "pub-4673636750726986";
-google_ad_width = 728;
-google_ad_height = 90;
-google_ad_format = "728x90_as";
-google_ad_type = "text";
-google_ad_channel ="";
-google_color_border = "333333";
-google_color_bg = "000000";
-google_color_link = "FFFFFF";
-google_color_url = "999999";
-google_color_text = "CCCCCC";
-//--></script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-	</script></td></tr></table>
+<tr><td class="main"></td><td id="google"><script language='JavaScript' type='text/javascript'>
+<!--
+   var m3_p = location.protocol=='https:'?'https:':'http:';
+   var m3_u = location.protocol=='https:'?'//banner.thebhg.org/www/delivery/ajs.php':'//banner.thebhg.org/www/delivery/ajs.php';
+   var m3_r = Math.floor(Math.random()*99999999999);
+   if (!document.phpAds_used) document.phpAds_used = ',';
+   document.write ("<" + "script language='JavaScript'" );
+   document.write ("type='text/javascript' src='");
+   document.write (m3_p + m3_u + "?n=a9b40495");
+   document.write ("&zoneid=1&cb=INSERT_RANDOM_NUMBER_HERE");
+   document.write ("&exclude=" + document.phpAds_used);
+   document.write ("&loc=" + escape(window.location));
+   if (document.referrer)
+     document.write ("&referer=" + escape(document.referrer));
+   document.write ('&r=' + m3_r);
+   document.write ("&ct0=" + escape(document.phpAds_ct0));
+   document.write ("'><" + "/script>");
+//--></script></td></tr></table>
 
 </body>
 </html>
