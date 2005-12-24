a9ad858a6fddd1681c38bf7db696e7d48cc96fc3
Adam Ashley <aashley@adamashley.name>
2005-12-24 04:21:23 +0000

    updated for final banner system

--- scum/template/footer.php
+++ scum/template/footer.php
@@ -2,22 +2,24 @@
 <tr><td id="bottom" colspan="2"></td></tr>
 <tr><td class="main"></td><td id="google"><script language='JavaScript' type='text/javascript'>
 <!--
-   var m3_p = location.protocol=='https:'?'https:':'http:';
-   var m3_u = location.protocol=='https:'?'//banner.thebhg.org/www/delivery/ajs.php':'//banner.thebhg.org/www/delivery/ajs.php';
-   var m3_r = Math.floor(Math.random()*99999999999);
-   if (!document.phpAds_used) document.phpAds_used = ',';
-   document.write ("<" + "script language='JavaScript'" );
-   document.write ("type='text/javascript' src='");
-   document.write (m3_p + m3_u + "?n=a9b40495");
-   document.write ("&zoneid=1&cb=INSERT_RANDOM_NUMBER_HERE");
-   document.write ("&exclude=" + document.phpAds_used);
-   document.write ("&loc=" + escape(window.location));
-   if (document.referrer)
-     document.write ("&referer=" + escape(document.referrer));
-   document.write ('&r=' + m3_r);
-   document.write ("&ct0=" + escape(document.phpAds_ct0));
-   document.write ("'><" + "/script>");
-//--></script></td></tr></table>
+// Insert click tracking URL here
+document.phpAds_ct0 ='Insert_Clicktrack_URL_Here'
+
+var awrz_rnd = Math.floor(Math.random()*99999999999);
+var awrz_protocol = location.protocol.indexOf('https')>-1?'https:':'http:';
+if (!document.phpAds_used) document.phpAds_used = ',';
+document.write ("<" + "script language='JavaScript' type='text/javascript' src='");
+document.write (awrz_protocol+"//banner.thebhg.org/adjs.php?n=a6ae4dab");
+document.write ("&zoneid=3");
+document.write ("&exclude=" + document.phpAds_used);
+document.write ("&loc=" + escape(window.location));
+if (document.referrer)
+  document.write ("&referer=" + escape(document.referrer));
+document.write ('&r=' + awrz_rnd);
+document.write ("&ct0=" + escape(document.phpAds_ct0));
+document.write ("'><" + "/script>");
+//-->
+</script><noscript><a href='http://banner.thebhg.org/adclick.php?n=a6ae4dab' target='_blank'><img src='http://banner.thebhg.org/adview.php?zoneid=3&n=a6ae4dab' border='0' alt=''></a></noscript></td></tr></table>
 
 </body>
 </html>
