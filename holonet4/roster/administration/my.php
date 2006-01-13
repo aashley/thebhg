de24f6b942f737b056ad23eaeb9614d16f30d71f
Adam Ashley <aashley@adamashley.name>
2006-01-13 03:57:59 +0000

    correct page class name

--- holonet4/roster/administration/my.php
+++ holonet4/roster/administration/my.php
@@ -2,7 +2,7 @@
 
 include_once 'objects/holonet/tab/bar.php';
 
-class page_roster_administration extends holonet_page {
+class page_roster_administration_my extends holonet_page {
 
 	public function __construct($trail) {
 
