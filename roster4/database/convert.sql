4b991d3589d0a27e1c0a6297c264f3fe6a0b18e0
Adam Ashley <aashley@adamashley.name>
2006-01-13 07:02:03 +0000

    add some status info throughout script

--- roster4/database/convert.sql
+++ roster4/database/convert.sql
@@ -1,12 +1,6 @@
 BEGIN;
 
--- Code IDs
-ALTER TABLE coders RENAME core_code;
-ALTER TABLE core_code DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE core_code CHANGE md5 hash CHAR(32) NOT NULL;
-UPDATE core_code SET hash = LOWER(hash);
-ALTER TABLE core_code ADD UNIQUE KEY(hash);
-
+\! echo Dropping Old Tables
 -- Drop old CS code
 DROP TABLE `cs_bonus_points`, `cs_classes`, `cs_field_options`, `cs_fields`, `cs_pending_fields`, `cs_sheet_fields`, `cs_sheets`, `cs_unused_xp`, `cs_used_xp`, `new_members`;
 -- Drop old Holonet Crap that shouldnt be here
@@ -19,8 +13,19 @@ DROP TABLE `hn_cadre_applications` ,
 		 `hn_pending_reasons` ,
 		 `hn_reports` ,
 		 `http_logins` ;
+-- Roster Black list
+DROP TABLE roster_blacklist;
+
+-- Code IDs
+\! echo core_code
+ALTER TABLE coders RENAME core_code;
+ALTER TABLE core_code DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE core_code CHANGE md5 hash CHAR(32) NOT NULL;
+UPDATE core_code SET hash = LOWER(hash);
+ALTER TABLE core_code ADD UNIQUE KEY(hash);
 
 -- Roster Bio Data
+\! echo roster_biographical_data
 ALTER TABLE `roster_biographical_data` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_biographical_data ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_biographical_data ADD COLUMN dateupdated DATETIME NOT NULL;
@@ -28,10 +33,8 @@ ALTER TABLE roster_biographical_data ADD COLUMN datedeleted DATETIME;
 UPDATE roster_biographical_data SET datecreated = NOW(), dateupdated = NOW();
 ALTER TABLE roster_biographical_data CHANGE image_url imageurl TEXT;
 
--- Roster Black list
-DROP TABLE roster_blacklist;
-
 -- Roster Cadres
+\! echo roster_cadre
 ALTER TABLE roster_cadres RENAME roster_cadre;
 ALTER TABLE `roster_cadre` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_cadre ADD COLUMN datecreated DATETIME NOT NULL;
@@ -47,6 +50,7 @@ UPDATE roster_cadre SET datedeleted = date_deleted WHERE date_deleted = 0;
 ALTER TABLE roster_cadre DROP COLUMN date_deleted;
 
 -- Roster Division Categories
+\! echo roster_category
 ALTER TABLE roster_division_categories RENAME roster_division_category;
 ALTER TABLE `roster_division_category` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_division_category ADD COLUMN datecreated DATETIME NOT NULL;
@@ -57,6 +61,7 @@ UPDATE roster_division_category SET datecreated = NOW(), dateupdated = NOW();
 ALTER TABLE roster_division_category CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Roster Divisions
+\! echo roster_division
 ALTER TABLE roster_divisions RENAME roster_division;
 ALTER TABLE `roster_division` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_division ADD COLUMN datecreated DATETIME NOT NULL;
@@ -67,20 +72,29 @@ UPDATE roster_division SET datedeleted = NOW() WHERE deleted = 1;
 ALTER TABLE roster_division CHANGE home_page_url homepageurl VARCHAR(200);
 
 -- Roster History
+\! echo roster_history
 ALTER TABLE roster_history RENAME history_event;
+\! echo " - Renamed"
 ALTER TABLE `history_event` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+\! echo " - Collation set"
 ALTER TABLE history_event ADD COLUMN datecreated DATETIME NOT NULL AFTER `type`;
+\! echo " - Added date_created"
 UPDATE history_event SET datecreated = FROM_UNIXTIME(`date`);
+\! echo " - Updated date_created"
 ALTER TABLE history_event DROP COLUMN `date`;
+\! echo " - Dropped date"
 ALTER TABLE history_event ADD COLUMN item4 VARCHAR(250) NOT NULL DEFAULT '' AFTER `item3`;
+\! echo " - Added item4"
 
 -- Roster New Member
+\! echo roster_new_person
 ALTER TABLE roster_new_members RENAME roster_new_person;
 ALTER TABLE `roster_new_person` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_new_person ADD COLUMN datecreated DATETIME NOT NULL;
 UPDATE roster_new_person SET datecreated = NOW();
 
 -- Roster Position
+\! echo roster_position
 ALTER TABLE `roster_position` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_position ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_position ADD COLUMN dateupdated DATETIME NOT NULL;
@@ -92,6 +106,7 @@ ALTER TABLE roster_position CHANGE istrainee trainee INT(1) NOT NULL;
 ALTER TABLE roster_position CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Roster Rank
+\! echo roster_rank
 ALTER TABLE `roster_rank` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_rank ADD COLUMN datecreated DATETIME NOT NULL;
 ALTER TABLE roster_rank ADD COLUMN dateupdated DATETIME NOT NULL;
@@ -104,6 +119,7 @@ ALTER TABLE roster_rank CHANGE manual_set manuallyset INT(1) NOT NULL;
 ALTER TABLE roster_rank CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Roster Person
+\! echo roster_roster
 ALTER TABLE roster_roster RENAME roster_person;
 ALTER TABLE `roster_person` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE roster_person ADD COLUMN dateupdated DATETIME NOT NULL;
@@ -122,6 +138,7 @@ ALTER TABLE roster_person CHANGE hasship ship INT(1) NOT NULL;
 
 
 -- MedalBoard Awarded Medals
+\! echo medalboard_award
 ALTER TABLE mb_awarded_medals RENAME medalboard_award;
 ALTER TABLE `medalboard_award` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_award ADD COLUMN datecreated DATETIME NOT NULL;
@@ -133,6 +150,7 @@ ALTER TABLE medalboard_award DROP COLUMN `date`;
 ALTER TABLE `medalboard_award` CHANGE `why` `why` TEXT NOT NULL;
 
 -- MedalBoard Medal Categories
+\! echo medalboard_category
 ALTER TABLE mb_medal_categories RENAME medalboard_category;
 ALTER TABLE `medalboard_category` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_category ADD COLUMN datecreated DATETIME NOT NULL;
@@ -141,6 +159,7 @@ ALTER TABLE medalboard_category ADD COLUMN datedeleted DATETIME;
 ALTER TABLE medalboard_category CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- MedalBoard Medal Groups
+\! echo medalboard_group
 ALTER TABLE mb_medal_groups RENAME medalboard_group;
 ALTER TABLE `medalboard_group` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_group ADD COLUMN datecreated DATETIME NOT NULL;
@@ -152,6 +171,7 @@ UPDATE medalboard_group,mb_medal_descriptions SET medalboard_group.html = mb_med
 DROP TABLE mb_medal_descriptions;
 
 -- MedalBoard Medal Names
+\! echo medalboard_medal
 ALTER TABLE mb_medal_names RENAME medalboard_medal;
 ALTER TABLE `medalboard_medal` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE medalboard_medal ADD COLUMN datecreated DATETIME NOT NULL;
@@ -160,6 +180,7 @@ ALTER TABLE medalboard_medal ADD COLUMN datedeleted DATETIME;
 ALTER TABLE medalboard_medal CHANGE `order` sortorder INT(4) NOT NULL;
 
 -- Library Books
+\! echo library_book
 ALTER TABLE library_books RENAME library_book;
 ALTER TABLE `library_book` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_book ADD COLUMN datecreated DATETIME NOT NULL;
@@ -168,6 +189,7 @@ ALTER TABLE library_book ADD COLUMN datedeleted DATETIME;
 ALTER TABLE library_book CHANGE image_type imagetype TEXT;
 
 -- Library Chapters
+\! echo library_chapter
 ALTER TABLE library_chapters RENAME library_chapter;
 ALTER TABLE `library_chapter` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_chapter ADD COLUMN datecreated DATETIME NOT NULL;
@@ -176,6 +198,7 @@ ALTER TABLE library_chapter ADD COLUMN datedeleted DATETIME;
 ALTER TABLE library_chapter CHANGE sort_order sortorder INT(11) NOT NULL;
 
 -- Library Moderators
+\! echo library_moderator
 ALTER TABLE library_moderators RENAME library_moderator;
 ALTER TABLE `library_moderator` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_moderator ADD COLUMN datecreated DATETIME NOT NULL;
@@ -183,6 +206,7 @@ ALTER TABLE library_moderator ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE library_moderator ADD COLUMN datedeleted DATETIME;
 
 -- Library Sections
+\! echo library_section
 ALTER TABLE library_sections RENAME library_section;
 ALTER TABLE `library_section` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_section ADD COLUMN datecreated DATETIME NOT NULL;
@@ -192,6 +216,7 @@ ALTER TABLE library_section CHANGE sort_order sortorder INT(11) NOT NULL;
 ALTER TABLE library_section CHANGE useHTML html INT(1) NOT NULL;
 
 -- Library Shelves
+\! echo library_shelves
 ALTER TABLE library_shelves RENAME library_shelf;
 ALTER TABLE `library_shelf` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE library_shelf ADD COLUMN datecreated DATETIME NOT NULL;
@@ -200,6 +225,7 @@ ALTER TABLE library_shelf ADD COLUMN datedeleted DATETIME;
 ALTER TABLE library_shelf CHANGE sort_order sortorder INT(11) NOT NULL;
 
 -- Newsboard
+\! echo news_item
 ALTER TABLE newsboard RENAME news_item;
 ALTER TABLE `news_item` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE news_item ADD COLUMN datecreated DATETIME NOT NULL;
@@ -208,6 +234,7 @@ ALTER TABLE news_item ADD COLUMN datedeleted DATETIME;
 UPDATE news_item SET datecreated = FROM_UNIXTIME(`timestamp`);
 ALTER TABLE news_item DROP COLUMN `timestamp`;
 
+\! echo college_reward
 CREATE TABLE `college_reward` (
 		`id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
 		`datecreated` DATETIME NOT NULL,
@@ -226,6 +253,7 @@ INSERT INTO `college_reward` (datecreated, dateupdated, exam, rewardtype, requir
 INSERT INTO `college_reward` (datecreated, dateupdated, exam, rewardtype, requiredscore, award) SELECT NOW(), NOW(), id, 'medal', 100, medal_award FROM ntc_exams WHERE medal_award != 0;
 
 -- NTC Exams
+\! echo college_exam
 ALTER TABLE ntc_exams RENAME college_exam;
 ALTER TABLE `college_exam` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_exam ADD COLUMN datecreated DATETIME NOT NULL;
@@ -237,6 +265,7 @@ ALTER TABLE college_exam DROP COLUMN medal_award;
 ALTER TABLE college_exam DROP COLUMN credit_award;
 
 -- NTC Questions
+\! echo college_exam_question
 ALTER TABLE ntc_exam_questions RENAME college_exam_question;
 ALTER TABLE `college_exam_question` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_exam_question ADD COLUMN datecreated DATETIME NOT NULL;
@@ -244,6 +273,7 @@ ALTER TABLE college_exam_question ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_exam_question CHANGE date_deleted datedeleted DATETIME;
 
 -- NTC Exam Markers
+\! echo college_exam_marker
 ALTER TABLE ntc_exam_markers RENAME college_exam_marker;
 ALTER TABLE `college_exam_marker` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_exam_marker ADD COLUMN datecreated DATETIME NOT NULL;
@@ -251,6 +281,7 @@ ALTER TABLE college_exam_marker ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_exam_marker ADD COLUMN datedeleted DATETIME;
 
 -- NTC Exam Completed
+\! echo college_submission
 ALTER TABLE ntc_exam_completed RENAME college_submission;
 ALTER TABLE `college_submission` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE college_submission ADD COLUMN datecreated DATETIME NOT NULL;
@@ -271,6 +302,7 @@ ALTER TABLE college_submission_answer ADD COLUMN dateupdated DATETIME NOT NULL;
 ALTER TABLE college_submission_answer CHANGE result submission INT(11) NOT NULL;
 
 -- Start Chart System
+\! echo starchart_system
 CREATE TABLE `starchart_system` (
 		`id` INT(11) NOT NULL auto_increment,
 		`datecreated` DATETIME NOT NULL,
@@ -284,6 +316,7 @@ CREATE TABLE `starchart_system` (
 INSERT INTO starchart_system (name, datecreated, dateupdated, datedeleted, description) VALUES ('Lyarna', NOW(), NOW(), NULL, 'The Lyarna system. Home to the BHG');
 
 -- Lyarna planets
+\! echo starchart_planet
 CREATE TABLE `starchart_planet` (
 		`id` int(11) NOT NULL auto_increment,
 		`datecreated` DATETIME NOT NULL,
@@ -313,6 +346,7 @@ CREATE TABLE `starchart_planet` (
 INSERT INTO starchart_planet SELECT `id`, NOW(), NOW(), NULL, 1, `name`, `pic`, `type`, `temp`, `atmo`, `hydro`, `gravity`, `terrain`, `day`, `year`, `species`, `starport`, `pop`, `tech`, `exp`, `imp`, `misc` FROM thebhg_lyarna.planets;
 
 -- Lyarna Sites
+\! echo starchart_site
 CREATE TABLE `starchart_site` (
 		`id` int(11) NOT NULL auto_increment,
 		`datecreated` DATETIME NOT NULL,
