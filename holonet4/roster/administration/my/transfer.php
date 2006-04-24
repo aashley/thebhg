1af8578a2d2d2d039f6db51453711ba8b3f6a465
Adam Ashley <aashley@adamashley.name>
2006-04-24 04:09:54 +0000

    Request transfer page

--- holonet4/roster/administration/my/transfer.php
+++ holonet4/roster/administration/my/transfer.php
@@ -18,6 +18,63 @@ class page_roster_administration_my_transfer extends holonet_page {
 
 		$this->setTitle('Request Transfer');
 
+		$user = $GLOBALS['bhg']->user;
+
+		$targets = $GLOBALS['bhg']->roster->getDivisions(array('category' => bhg_roster::getDivisionCategory(2)));
+
+		// Add Retirees
+		$targets->items[] = 2;
+
+		$options = array();
+
+		foreach ($targets as $target) {
+
+			$options[$target->getID()] = $targets->getName();
+
+		}
+
+		$form = new holonet_form('my_tranfer');
+
+		$form->addElement('select',
+				'target',
+				'Transfer To:',
+				$options);
+
+		$form->addButtons('Request Transfer');
+
+		$form->setDefaults(
+				array(
+					'target'	=> $user->getDivision()->getID(),
+					)
+				);
+
+		if ($form->validate()) {
+
+			$values = $form->exportValues();
+
+			try {
+
+				$target = bhg_roster::getDivision($values['target']);
+
+				$this->addBodyContent('<p>Requesting transfer to '.$target->getName().'... ');
+
+				$user->requestTransfer($target);
+
+				$this->addBodyContent('Success.</p>');
+
+			} catch (bhg_fatal_exception $e) {
+
+				$this->addBodyContent('Failure.</p>');
+				$this->addBodyContent($e->__toString());
+
+			}
+
+		} else {
+
+			$this->addBodyContent($form);
+
+		}
+
 		$this->addSideMenu($GLOBALS['holonet']->roster->getAdministrationMenu());
 
 	}
