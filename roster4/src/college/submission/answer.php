6d1f87c652e3e2794de9810861ddd035a000a49c
Adam Ashley <aashley@adamashley.name>
2006-05-01 07:38:25 +0000

    Fix C&P coding errors

--- roster4/src/college/submission/answer.php
+++ roster4/src/college/submission/answer.php
@@ -30,8 +30,8 @@ class bhg_college_submission_answer extends bhg_core_base {
 	public function __construct($id) {
 		parent::__construct('college_submission_answer', $id);
 		$this->__addFieldMap(array(
-					'submission' => 'college_submission',
-					'question' => 'college_exam_question',
+					'submission' => 'bhg_college_submission',
+					'question' => 'bhg_college_exam_question',
 					));
 		$this->__addDefaultCodePermissions('set', 'god');
 		$this->__addDefaultCodePermissions('get', 'god');
