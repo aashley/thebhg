f721f06ab151453e156fb9cad0ed4a6ef46c1f2d
Adam Ashley <aashley@adamashley.name>
2006-05-01 06:59:44 +0000

    set default templates for form

--- holonet4/objects/holonet/form.php
+++ holonet4/objects/holonet/form.php
@@ -11,6 +11,35 @@ class holonet_form extends HTML_QuickForm {
 		$this->removeAttribute('name');
 		$this->registerElementType('personselect', 'objects/holonet/form/person.php', 'holonet_form_person');
 
+		$renderer = $this->defaultRenderer();
+
+		$renderer->setHeaderTemplate("\n"
+				."\t<tr>\n"
+				."\t\t<th style=\"white-space: nowrap;\" colspan=\"2\">{header}</th>\n"
+				."\t</tr>\n");
+
+		$renderer->setElementTemplate("\n"
+				."\t<tr>\n"
+				."\t\t<td align=\"right\" valign=\"top\"><!-- BEGIN required --><span style=\"color: #ff0000\">*</span><!-- END required -->{label}</td>\n"
+				."\t\t<td valign=\"top\" align=\"left\"><!-- BEGIN error --><span style=\"color: #ff0000\">{error}</span><br /><!-- END error -->\t{element}</td>\n"
+				."\t</tr>");
+
+		$renderer->setFormTemplate("\n"
+				."<form{attributes}>\n"
+				."<div>\n"
+				."{hidden}"
+				."<table class=\"form\">\n"
+				."{content}\n"
+				."</table>\n"
+				."</div>\n"
+				."</form>");
+
+		$renderer->setRequiredNoteTemplate("\n"
+				."\t<tr>\n"
+				."\t\t<td></td>\n"
+				."\t\t<td align=\"left\" valign=\"top\">{requiredNote}</td>\n"
+				."\t</tr>");
+
 	}
 
 	public function addButtons($submitText = null) {
