9b1a555c65d12255bbe97c5d8ba0f7e8392d87a0
Adam Ashley <aashley@adamashley.name>
2006-05-17 06:26:50 +0000

    debug

--- holonet4/library/module.php
+++ holonet4/library/module.php
@@ -24,6 +24,7 @@ class holonet_module_library extends holonet_module {
 
 		$menus = array();
 
+		print 'finding stuff';
 		foreach ($GLOBALS['bhg']->library->getShelves() as $shelf) {
 			
 			$menu = new holonet_menu;
