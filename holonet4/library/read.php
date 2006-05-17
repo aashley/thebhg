99d25740cd45266b1e3da413e73282e23c4f4ffc
Adam Ashley <aashley@adamashley.name>
2006-05-17 07:15:02 +0000

    Add rendering of books and chapters

--- holonet4/library/read.php
+++ holonet4/library/read.php
@@ -10,15 +10,59 @@ class page_library_read extends holonet_page {
 
 		if ($obj instanceof bhg_library_book) {
 
-			$this->addBodyContent('<p>Render book.</p>');
+			$this->renderBook($obj);
 
 		} elseif ($obj instanceof bhg_library_chapter) {
 
-			$this->addBodyContent('<p>Render chapter.</p>');
+			$this->renderChapter($obj);
 
 		}
 
 		$this->addSideMenu($GLOBALS['holonet']->library->getBookMenu());
+
+	}
+
+	public function renderBook(bhg_library_book $book) {
+
+		$this->setTitle($book->getName());
+
+		$this->addBodyContent('<div class="center"><img src="/library/image/'.$book->getID().'" alt="Book Cover Image" /></div>');
+
+		$this->addBodyContent('<p>'.$book->getDescription().'</p>');
+
+		$chapters = $book->getChapters();
+
+		if ($chapters->count() > 0) {
+
+			$this->addBodyContent('<ol>');
+
+			foreach ($chapters as $chapter) {
+
+				$this->addBodyContent('<li>'.holonet::output($chapter).'</li>');
+
+			}
+
+			$this->addBodyContent('<ol>');
+
+		}
+
+	}
+
+	public function renderChapter(bhg_library_chapter $chapter) {
+
+		$this->setTitle($chapter->getName());
+
+		$sections = $chapter->getSections();
+
+		if ($sections->count() > 0) {
+
+			foreach ($sections as $section) {
+
+				$this->addBodyContent('<p>'.holonet::output($section).'</p>');
+
+			}
+
+		}
 
 	}
 
