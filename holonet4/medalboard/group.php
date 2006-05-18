0f68c6b52ca5e22da618ffdb71d34587e55554b0
Adam Ashley <aashley@adamashley.name>
2006-05-18 01:12:55 +0000

    Add display of awarded medals to medal groups page

--- holonet4/medalboard/group.php
+++ holonet4/medalboard/group.php
@@ -11,9 +11,43 @@ class page_medalboard_group extends holonet_page {
 
 		$group = bhg_medalboard::getGroup($id);
 
-		$this->setTitle($group->getName());
+		$this->setTitle('Medal :: '.$group->getName());
 
 		$this->addBodyContent($group->getDescription());
+
+		$table = new HTML_Table();
+
+		$head = $table->getHeader();
+		$body = $table->getBody();
+
+		$head->addRow(
+				array(
+					'Date',
+					'Recipient',
+					'Awarder',
+					'Medal',
+					'Reason',
+					),
+				array(),
+				'TH');
+
+		$awards = $group->getAwards();
+
+		foreach ($awards as $award) {
+
+			$body->addRow(
+					array(
+						$award->getDateCreated(),
+						holonet::output($award->getRecipient()),
+						holonet::output($award->getAwarder()),
+						holonet::output($award->getMedal()),
+						htmlspecialchars($award->getReason()),
+						)
+					);
+
+		}
+
+		$this->addBodyContent($table);
 
 		$this->addSideMenu($GLOBALS['holonet']->medalboard->getSideMenu());
 
