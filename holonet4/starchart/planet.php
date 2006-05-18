7fcb6a5f41a5eb952e058cdbbb99bf9ac4643194
Adam Ashley <aashley@adamashley.name>
2006-05-18 08:16:55 +0000

    Add statistics tab

--- holonet4/starchart/planet.php
+++ holonet4/starchart/planet.php
@@ -16,6 +16,7 @@ class page_starchart_planet extends holonet_page {
 		$bar = new holonet_tab_bar;
 
 		$bar->addTab($this->buildDescription($planet));
+		$bar->addTab($this->buildStats($planet));
 		$bar->addTab($this->buildSites($planet));
 
 		$this->addBodyContent($bar);
@@ -33,6 +34,36 @@ class page_starchart_planet extends holonet_page {
 		$tab->addContent('<img src="http://lyarna.thebhg.org/planets/images/'.$planet->getPicture().'" alt="" style="float: left;" />');
 
 		$tab->addContent('<p>'.$planet->getMisc().'</p>');
+
+		return $tab;
+
+	}
+
+	public function buildStats($planet) {
+
+		$tab = new holonet_tab('stats', 'Statistics');
+
+		$table = new HTML_Table();
+
+		$body = $table->getBody();
+
+		$body->addRow(array('Name:', $planet->getName()));
+		$body->addRow(array('Type:', $planet->getType()));
+		$body->addRow(array('Temperature:', $planet->getTemperature()));
+		$body->addRow(array('Atmosphere:', $planet->getAtmosphere()));
+		$body->addRow(array('Hydrosphere:', $planet->getHydrosphere()));
+		$body->addRow(array('Gravity:', $planet->getGravity()));
+		$body->addRow(array('Terrain:', $planet->getTerrain()));
+		$body->addRow(array('Day:', $planet->getDay()));
+		$body->addRow(array('Year:', $planet->getYear()));
+		$body->addRow(array('Species:', $planet->getSpecies()));
+		$body->addRow(array('Starport:', $planet->getStarport()));
+		$body->addRow(array('Population:', $planet->getPopulation()));
+		$body->addRow(array('Technology:', $planet->getTechnology()));
+		$body->addRow(array('Exports:', $planet->getExports()));
+		$body->addRow(array('Imports:', $planet->getImports()));
+
+		$tab->addContent($table);
 
 		return $tab;
 
