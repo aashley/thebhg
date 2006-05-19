16ecce22e86ae596261b49fcb05391316fb1d16a
Eric Garside <eric.garside@knewton.com>
2006-05-19 05:44:18 +0000

    added TCL 'status' function for kags

--- tactician/kag/tcl.php
+++ tactician/kag/tcl.php
@@ -93,61 +93,75 @@ else {
 				$value = 168;
 		}
 
-		$plebs = $roster->SearchPosition($value);
-		if (!$plebs) {
-			$plebs = $roster->SearchName($value);
+		if (strtolower($value) == 'status'){
+			$result = mysql_query('SELECT id FROM kag_events WHERE end >= '.time().' AND start <= '.time().' AND kag=' . $kag->GetID(), $db);
+			
+			echo 'KAG ' . roman($kag->getID()) . ': ';
+			
+			while ($info = mysql_fetch_assoc($result)){
+				$event = $ka->getEvent($info['id']);
+				echo '[' . $event->getName() . ' - ' . format_time($event->GetEnd() - time(), FT_SECOND) . '] ';
+			}
+			
+			echo "\n";
+					
+		} else {
+			$plebs = $roster->SearchPosition($value);
 			if (!$plebs) {
-				$plebs = $roster->SearchIRCNick($value);
-			}
-		}
-
-		if (is_array($plebs) && count($plebs) > 0) {
-			foreach ($plebs as $pleb) {
-				$div = $pleb->GetDivision();
-				$result = mysql_query('SELECT person FROM kag_signups WHERE person=' . $pleb->GetID(), $db);
-				if ($result && mysql_num_rows($result) && $div->GetID() != 16 && $div->GetID() != 0) {
-					$new_plebs[] = $pleb;
+				$plebs = $roster->SearchName($value);
+				if (!$plebs) {
+					$plebs = $roster->SearchIRCNick($value);
 				}
 			}
-		}
-
-		$plebs = $new_plebs;
-		
-		if (count($plebs) > 3) {
-			echo 'More than three hunters match the name given.';
-		}
-		elseif (count($plebs) == 0) {
-			echo 'No hunters match the criteria given.';
-		}
-		else {
-			$info = array();
-			foreach ($plebs as $pleb) {
-				$result = mysql_query('SELECT kabal FROM kag_signups WHERE person=' . $pleb->GetID() . ' GROUP BY kabal', $db);
-				$kabals = array();
-				while ($row = mysql_fetch_array($result)) {
-					$kabal = $roster->GetKabal($row['kabal']);
-					$kabals[$kabal->GetID()] = $kabal->GetName();
+	
+			if (is_array($plebs) && count($plebs) > 0) {
+				foreach ($plebs as $pleb) {
+					$div = $pleb->GetDivision();
+					$result = mysql_query('SELECT person FROM kag_signups WHERE person=' . $pleb->GetID(), $db);
+					if ($result && mysql_num_rows($result) && $div->GetID() != 16 && $div->GetID() != 0) {
+						$new_plebs[] = $pleb;
+					}
 				}
-				asort($kabals);
-				$result = mysql_query('SELECT MIN(kag) AS first, MAX(kag) AS last, SUM(points) AS points, COUNT(DISTINCT id) AS events FROM kag_signups WHERE person=' . $pleb->GetID(), $db);
-				$row = mysql_fetch_array($result);
-				$hinfo = 'KG History for ' . $pleb->GetName() . ' (' . implode(', ', $kabals) . '): ';
-				if ($row['first'] == $row['last']) {
-					$hinfo .= 'KG ' . roman($row['first']);
-				}
-				else {
-					$hinfo .= 'KG ' . roman($row['first']) . ' - KG ' . roman($row['last']);
-				}
-				$hinfo .= '; ' . number_format($row['points']) . ' points; ' . number_format($row['events']) . ' events; ';
-				$dnp_result = mysql_query('SELECT COUNT(DISTINCT id) AS dnps FROM kag_signups WHERE state=2 AND person=' . $pleb->GetID(), $db);
-				if ($dnp_result && mysql_num_rows($dnp_result)) {
-					$dnps = mysql_result($dnp_result, 0, 'dnps');
-					$hinfo .= number_format($dnps) . ' DNP' . ($dnps != 1 ? 's' : '') . '; ';
-				}
-				$hinfo .= number_format($row['points'] / $row['events'], 1) . ' points per event.';
-				$info[] = $hinfo;
 			}
-			echo implode("\n", $info);
+	
+			$plebs = $new_plebs;
+			
+			if (count($plebs) > 3) {
+				echo 'More than three hunters match the name given.';
+			}
+			elseif (count($plebs) == 0) {
+				echo 'No hunters match the criteria given.';
+			}
+			else {
+				$info = array();
+				foreach ($plebs as $pleb) {
+					$result = mysql_query('SELECT kabal FROM kag_signups WHERE person=' . $pleb->GetID() . ' GROUP BY kabal', $db);
+					$kabals = array();
+					while ($row = mysql_fetch_array($result)) {
+						$kabal = $roster->GetKabal($row['kabal']);
+						$kabals[$kabal->GetID()] = $kabal->GetName();
+					}
+					asort($kabals);
+					$result = mysql_query('SELECT MIN(kag) AS first, MAX(kag) AS last, SUM(points) AS points, COUNT(DISTINCT id) AS events FROM kag_signups WHERE person=' . $pleb->GetID(), $db);
+					$row = mysql_fetch_array($result);
+					$hinfo = 'KG History for ' . $pleb->GetName() . ' (' . implode(', ', $kabals) . '): ';
+					if ($row['first'] == $row['last']) {
+						$hinfo .= 'KG ' . roman($row['first']);
+					}
+					else {
+						$hinfo .= 'KG ' . roman($row['first']) . ' - KG ' . roman($row['last']);
+					}
+					$hinfo .= '; ' . number_format($row['points']) . ' points; ' . number_format($row['events']) . ' events; ';
+					$dnp_result = mysql_query('SELECT COUNT(DISTINCT id) AS dnps FROM kag_signups WHERE state=2 AND person=' . $pleb->GetID(), $db);
+					if ($dnp_result && mysql_num_rows($dnp_result)) {
+						$dnps = mysql_result($dnp_result, 0, 'dnps');
+						$hinfo .= number_format($dnps) . ' DNP' . ($dnps != 1 ? 's' : '') . '; ';
+					}
+					$hinfo .= number_format($row['points'] / $row['events'], 1) . ' points per event.';
+					$info[] = $hinfo;
+				}
+				echo implode("\n", $info);
+			}
 		}
 	}
 }
