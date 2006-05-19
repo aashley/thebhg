e8a2654a3bd192cae342feca18773efc6da0ea87
Adam Ashley <aashley@adamashley.name>
2006-05-19 01:51:27 +0000

    more detailed display of sites

--- holonet4/starchart/site.php
+++ holonet4/starchart/site.php
@@ -13,11 +13,50 @@ class page_starchart_site extends holonet_page {
 
 		$this->setTitle('Site :: '.$site->getName());
 
-		$this->addBodyContent('<p>'.$site->getMisc().'</p>');
+		$bar = new holonet_tab_bar;
+
+		$bar->addTab($this->buildDescription($site));
+		$bar->addTab($this->buildStats($site));
+
+		$this->addBodyContent($bar);
 
 		$this->addSideMenu($GLOBALS['holonet']->starchart->getPlanetMenu($site->getPlanet()));
 		$this->addSideMenu($GLOBALS['holonet']->starchart->getSystemMenu($site->getPlanet()->getSystem()));
 		$this->addSideMenu($GLOBALS['holonet']->starchart->getSideMenu());
+
+	}
+
+	public function buildDescription(bhg_starchart_site $site) {
+
+		$tab = new holonet_tab('description', 'Description');
+
+		if (strlen($site->getPicture()) > 0)
+			$tab->addContent('<img src="'.$site->getPicture().'" alt="Site Picture" style="float: left;" />');
+
+		$tab->addContent('<p>'.$site->getMisc().'</p>');
+
+		return $tab;
+
+	}
+
+	public function buildStats(bhg_starchart_site $site) {
+
+		$tab = new holonet_tab('stats', 'Statistics');
+
+		$table = new HTML_Table();
+
+		$body = $table->getBody();
+
+		$body->addRow(array('Name:', holonet::output($site)));
+		$body->addRow(array('Owner:', holonet::output($site->getOwner())));
+		$body->addRow(array('Location Type:', ucfirst($site->getSiteType())));
+		$body->addRow(array('Location:', $site->getLocation())));
+		$body->addRow(array('Type:', $site->getType())));
+		$body->addRow(array('Arena Permitted:', ($site->isArena() ? 'Yes' : 'No')));
+
+		$tab->addContent($body);
+
+		return $tab;
 
 	}
 
