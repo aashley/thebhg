29d0acab732a07b904501f76d7ca30c65861f7ac
Adam Ashley <aashley@adamashley.name>
2006-05-19 01:46:46 +0000

    Add starchart_site::getOwner() function

--- roster4/src/starchart/site.php
+++ roster4/src/starchart/site.php
@@ -30,9 +30,53 @@ class bhg_starchart_site extends bhg_core_base {
 	public function __construct($id) {
 		parent::__construct('starchart_site', $id);
 		$this->__addFieldMap(array(
-					'planet' => 'bhg_starchart_planet',
+					'planet'		=> 'bhg_starchart_planet',
+					'position'	=> 'bhg_roster_position',
+					'division'	=> 'bhg_roster_division',
+					'person'		=> 'bhg_roster_person',
 					));
+		$this->__addBooleanFields(array('arena'));
 		$this->__addDefaultCodePermissions('set', 'god');
+	}
+
+	// }}}
+	// {{{ getOwner()
+	
+	/**
+	 * Returns the owner of this property
+	 *
+	 * @return string|bhg_roster_person
+	 */
+	public function getOwner() {
+
+		if (	 $this->data['position'] == 0
+				&& $this->data['division'] == 0
+				&& $this->data['person'] == 0)
+			return $this->data['owner'];
+
+		if ($this->data['person'] > 0)
+			return bhg_roster::getPerson($this->data['person']);
+
+		$filters = array();
+
+		if ($this->data['position'] > 0)
+			$filters['position'] = bhg_roster::getPosition($this->data['position']);
+
+		if ($this->data['division'] > 0)
+			$filters['division'] = bhg_roster::getDivision($this->data['division']);
+
+		$possibles = $GLOBALS['bhg']->roster->getPeople($filters);
+
+		if ($possibles->count() > 0) {
+
+			return $possibles->getItem();
+
+		} else {
+
+			return "Unknown Owner";
+
+		}
+
 	}
 
 	// }}}
