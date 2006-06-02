af348dd86de19700d4bcefd4b8be5156f957345f
Adam Ashley <aashley@adamashley.name>
2006-06-02 02:03:52 +0000

    Basic viewing of new star chart

--- holonet4/starchart/module.php
+++ holonet4/starchart/module.php
@@ -20,48 +20,33 @@ class holonet_module_starchart extends holonet_module {
 
 		}
 
+		if (is_numeric($trail[0])) {
+
+			include_once 'starchart/view.php';
+			return new page_starchart_view(array($trail[0]));
+
+		}
+
 		// This breaks the URL spec, as + only has meaning within a GET parameter,
 		// but it's (a) useful, and (b) somewhat expected.
 		$trail[0] = str_replace('+', ' ', $trail[0]);
 
-		$systems = $GLOBALS['bhg']->starchart->getSystems(array('name' => $trail[0]));
-		$planets = $GLOBALS['bhg']->starchart->getPlanets(array('name' => $trail[0]));
-		$sites = $GLOBALS['bhg']->starchart->getSites(array('name' => $trail[0]));
+		$objects = $GLOBALS['bhg']->starchart->getObjects(array('name' => $trail[0]));
 
-		if (	 $systems->count() == 0
-				&& $planets->count() == 0
-				&& $sites->count() == 0) {
+		if ($objects->count() == 0) {
 
 			include_once 'starchart/notfound.php';
 			return new page_starchart_notfound($trail);
 
-		} elseif (	 $systems->count() == 1
-							&& $planets->count() == 0
-							&& $sites->count() == 0) {
+		} elseif ($objects->count() == 1) {
 
-			include_once './starchart/system.php';
-			return new page_starchart_system(array($systems->getItem(0)->getID()));
-
-		} elseif (	 $systems->count() == 0
-							&& $planets->count() == 1
-							&& $sites->count() == 0) {
-
-			include_once './starchart/planet.php';
-			return new page_starchart_planet(array($planets->getItem(0)->getID()));
-
-		} elseif (	 $systems->count() == 0
-							&& $planets->count() == 0
-							&& $sites->count() == 1) {
-
-			include_once './starchart/site.php';
-			return new page_starchart_site(array($sites->getItem(0)->getID()));
+			include_once './starchart/view.php';
+			return new page_starchart_view(array($objects->getItem(0)->getID()));
 
 		} else {
 
 			include_once 'starchart/disambig.php';
-			$trail[] = $systems;
-			$trail[] = $planets;
-			$trail[] = $sites;
+			$trail[] = $objects;
 			return new page_starchart_disambig($trail);
 
 		}
@@ -73,11 +58,11 @@ class holonet_module_starchart extends holonet_module {
 		$menus = array();
 
 		$menu = new holonet_menu;
-		$menu->title = 'Systems';
+		$menu->title = 'Global Items';
 
-		foreach ($GLOBALS['bhg']->starchart->getSystems() as $system) {
+		foreach ($GLOBALS['bhg']->starchart->getObjects() as $object) {
 			
-			$menu->addItem(new holonet_menu_item($system->getName(), '/starchart/system/'.$system->getID()));
+			$menu->addItem(new holonet_menu_item($object->getName(), '/starchart/'.$object->getID()));
 
 		}
 
@@ -87,35 +72,16 @@ class holonet_module_starchart extends holonet_module {
 
 	}
 
-	public function getSystemMenu(bhg_starchart_system $system) {
+	public function getObjectMenu(bhg_starchart_object $object) {
 
 		$menus = array();
 
 		$menu = new holonet_menu;
-		$menu->title = $system->getName();
+		$menu->title = $object->getName();
 
-		foreach ($system->getPlanets() as $planet) {
+		foreach ($object->getChildren() as $child) {
 
-			$menu->addItem(new holonet_menu_item($planet->getName(), '/starchart/planet/'.$planet->getID()));
-
-		}
-
-		$menus[] = $menu;
-
-		return $menus;
-
-	}
-
-	public function getPlanetMenu(bhg_starchart_planet $planet) {
-
-		$menus = array();
-
-		$menu = new holonet_menu;
-		$menu->title = $planet->getName();
-
-		foreach ($planet->getSites() as $site) {
-
-			$menu->addItem(new holonet_menu_item($site->getName(), '/starchart/site/'.$site->getID()));
+			$menu->addItem(new holonet_menu_item($child->getName(), '/starchart/'.$child->getID()));
 
 		}
 
@@ -136,35 +102,15 @@ class holonet_module_starchart extends holonet_module {
 											'Search For:',
 											array('size' => 20));
 
-		$form->addElement('select',
-											'in',
-											'Search In:',
-											array(
-												'system'	=> 'Systems',
-												'planet'	=> 'Planets',
-												'site'		=> 'Sites',
-												),
-											array(
-												'size' 			=> 3,
-												'multiple'	=> 'multiple',
-												)
-											);
-
 		$form->addButtons('Search');
 
 		$form->addRule('for',
 									 'A search term must be provided.',
 									 'required');
 
-		$form->addRule('in',
-									 'A search context must be provided.',
-									 'required');
-
 		$defaults = array();
 		if (isset($_GET['for']))
 			$defaults['for'] = $_GET['for'];
-		if (isset($_GET['in']))
-			$defaults['in'] = $_GET['in'];
 		$form->setDefaults($defaults);
 
 		$tab->addContent($form);
---
+++ holonet4/starchart/view.php
@@ -0,0 +1,79 @@
+<?php
+
+class page_starchart_view extends holonet_page {
+
+	public function buildPage() {
+
+		$this->pageBuilt = true;
+
+		$trail = $this->getTrailingElements();
+		$id = $trail[0];
+
+		$object = bhg_starchart::getObject($id);
+
+		$this->setTitle($object->getType()->getName().' :: '.$object->getName());
+
+		$bar = new holonet_tab_bar;
+
+		$bar->addTab($this->buildDescription($object));
+		$bar->addTab($this->buildStats($object));
+		$bar->addTab($this->buildSites($object));
+
+		$this->addBodyContent($bar);
+
+		$this->addSideMenu($GLOBALS['holonet']->starchart->getObjectMenu($object));
+		$this->addSideMenu($GLOBALS['holonet']->starchart->getSystemMenu($object->getSystem()));
+		$this->addSideMenu($GLOBALS['holonet']->starchart->getSideMenu());
+
+	}
+
+	public function buildDescription($object) {
+
+		$tab = new holonet_tab('description', 'Description');
+
+		$tab->addContent('<img src="'.$object->getPicture().'" alt="" style="float: left;" />');
+
+		$tab->addContent('<p>'.$object->getDescription().'</p>');
+
+		return $tab;
+
+	}
+
+	public function buildStats($object) {
+
+		$tab = new holonet_tab('stats', 'Statistics');
+
+		$table = new HTML_Table();
+
+		$body = $table->getBody();
+
+		$attributes = $object->getAttributes();
+
+		foreach ($attributes as $attribute) {
+
+			$body->addRow(array(
+						htmlspecialchars($attribute->getType()->getName()),
+						htmlspecialchars($attribute->getValue()),
+						));
+
+		}
+
+		$tab->addContent($table);
+
+		return $tab;
+
+	}
+
+	public function buildSites($object) {
+
+		$tab = new holonet_tab('sites', 'Objects');
+
+		$tab->addContent(holonet::buildList($object->getChildren()));
+
+		return $tab;
+
+	}
+
+}
+
+?>
