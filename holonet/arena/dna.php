55978fce396fadc774163a03c65c81dafdde9a30
Eric Garside <eric.garside@knewton.com>
2006-06-07 22:51:37 +0000

    Debugging

--- holonet/arena/dna.php
+++ holonet/arena/dna.php
@@ -5,9 +5,8 @@ include_once('citadel.inc');
 include_once('library.inc');
 include_once('objects/arena.php');
 
-echo 'hi';
-
 $character = new Character($_REQUEST['who']); 
+print_r($character);
 $character->DNA();
 
 ?>
