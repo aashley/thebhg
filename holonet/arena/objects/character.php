82eecf393dc85d7551b7799844fab41ead956119
Eric Garside <eric.garside@knewton.com>
2006-06-07 22:40:39 +0000

    addding dna files

--- holonet/arena/objects/character.php
+++ holonet/arena/objects/character.php
@@ -976,18 +976,18 @@
 		$this->UpdateCache();
 		
     	$project = $this->project;
-    	
+    	echo '**DNA**';
     	if ($this->HasValue($look, $col, $id)){
 	    	foreach ($this->ModFields($project) as $i){
-				echo '**DNA**::STATS';
 		    	foreach($this->GetStats($i) as $stat){
+			    	echo '::STATS';
 			    	if ($this->Permit(1, $stat->GetID(), $project)){
 				    	if ($stat->isInt())
 			    			echo '::' . $stat->GetName() . '::' . $this->Point($stat->GetID(), 'SYSTEM', $look, $id, $col);
 		    		}
 		    	}
-		    	echo '||SKILLS';
 		    	foreach($this->GetSkills($i) as $skill){
+			    	echo '||SKILLS';
 			    	if ($this->Permit(2, $skill->GetID(), $project)){
 			    		echo '||'.$skill->GetName() . '||' . $this->GetValue($skill->GetID(), $look, 'SYSTEM', $id, $col);
 		    		}
