ac3a834608841869480fd60e9ddf7e4cec6155b3
Eric Garside <eric.garside@knewton.com>
2006-06-07 22:56:49 +0000

    addding dna files

--- holonet/arena/dna.php
+++ holonet/arena/dna.php
@@ -3,12 +3,19 @@
 include_once('roster.inc');
 include_once('citadel.inc');
 include_once('library.inc');
-include_once('/objects/arena.php');
+$include = array('arena_core', 'tournament', 'sheet_module', 'character', 'field', 'npc_utilities', 'object', 'parse_npc', 'skill', 'statribute', 'creature', 'auxillary');
 
-$a = new Arena();
+foreach ($include as $pages){
+	$pages = $pages.'.php';
+	//echo $pages;
+	if (file_exists('objects/'.$pages)){
+		//echo ' got in.';
+		include_once $pages;
+	}
+	//echo '<br />';
+}
 
 $character = new Character($_REQUEST['who']); 
-print_r($character);
 $character->DNA();
 
 ?>
