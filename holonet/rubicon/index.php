b6fc40c31491f746280bd822d3bf9c0fe640b24d
Eric Garside <eric.garside@knewton.com>
2006-06-14 03:27:38 +0000

    rubicon project

--- holonet/rubicon/index.php
+++ holonet/rubicon/index.php
@@ -20,7 +20,15 @@ function output() {
 <!--
 var yes = 1;
 var pause = 0;
-var text = "<tt>holonet::__loadBootModule('/specialist/rubicon.ijc')<br />...<br />...<br />...<br />Done<br /><br />holonet::__cacheHold('/specialist/traceroute.iio', 'trcrt')<br /><br />...<br />...<br />...<br />Done<br /><br />execute rubiconTerminal.kib<br />...<br />Done<br /><br /><br /><br />Welcome to the Rubicon Terminal, root.<br /><br />executeCache('trcrt')<br /><br />Executing the TraceRoute Program.<br />...<br />...<br />Please Wait<br />...<br />...<br />...<br />...<be />..."
+<?php
+if (isset($_COOKIE['bhg_nhashafvasl'])){
+?>
+	var text = "<tt>holonet::__loadCache(\'/local.trcrt.log\')";
+} else {
+?>
+	var text = "<tt>holonet::__loadBootModule('/specialist/rubicon.ijc')<br />...<br />...<br />...<br />Done<br /><br />holonet::__cacheHold('/specialist/traceroute.iio', 'trcrt')<br /><br />...<br />...<br />...<br />Done<br /><br />execute rubiconTerminal.kib<br />...<br />Done<br /><br /><br /><br />Welcome to the Rubicon Terminal, root.<br /><br />executeCache('trcrt')<br /><br />Executing the TraceRoute Program.<br />...<br />...<br />Please Wait<br />...<br />...<br />...<br />...<be />..."
+<? } ?>
+
 startTyping(text, 50, "textDestination");
 //-->
 	</script><?
--- holonet/rubicon/trace.php
+++ holonet/rubicon/trace.php
@@ -41,37 +41,37 @@ if (isset($_REQUEST['hhcbqa'])){
 	echo "var speed = 0\n";
 	echo "var yes = 0;\n";
 	echo "var keep = 1;\n";
-	echo 'var text = "#file <br /># local.trcrt.log<br /><br />[122.1239.1235.123] holonet_terminal{kasa}_solrahl<br />[127.0.0.1] routing_trml{asa}_pltalon<br />[12841.1231.21418.212] subspace_router{jha}_firrerre<br />[182.2125.231.1231] subspace_router{jga}_moorja<br />[12312.734.862.258] holonet_terminal{kkd}_duro<br />[127.215.1927.1952] holonet_terminal{rra}_kuat<br />[17123.1241.73245.1235] subspace_router{laa}_zeltros<br />[217.1285.123.731587] routing_trml{jha}_toydaria<br />[1581.1925.8341.8123:8322] server_core{KVA}_shadda"';
+	echo 'var text = "<tt>#file <br /># local.trcrt.log<br /><br />[122.1239.1235.123] holonet_terminal{kasa}_solrahl<br />[127.0.0.1] routing_trml{asa}_pltalon<br />[12841.1231.21418.212] subspace_router{jha}_firrerre<br />[182.2125.231.1231] subspace_router{jga}_moorja<br />[12312.734.862.258] holonet_terminal{kkd}_duro<br />[127.215.1927.1952] holonet_terminal{rra}_kuat<br />[17123.1241.73245.1235] subspace_router{laa}_zeltros<br />[217.1285.123.731587] routing_trml{jha}_toydaria<br />[1581.1925.8341.8123:8322] server_core{KVA}_shadda"';
 } elseif (isset($_REQUEST['yad'])){
 
 	if ($_REQUEST['yad'] == '2552-1231-235-233'){
 		echo "var cernun = '&hhcbqa'\n";
-		echo 'var text = "passcode accepted<br /><br />Tracing route<br />...<br />...<br />...<br />...<br />...<br />[127.215.1927.1952] holonet_terminal{rra}_kuat<br />[17123.1241.73245.1235] subspace_router{laa}_zeltros<br />[217.1285.123.731587] routing_trml{jha}_toydaria<br />[1581.1925.8341.8123:8322] server_core{KVA}_shadda<br />...<br />...<br />...<br />trace complete<br /><br />holonet::__loadSpike(\'/specialist/spike4.spk\')<br /><br />spikeRout protocol initiated<br />...<br />...<br />...<br />Done<br /><br />holonet::__loadCache(\'/local.trcrt.log\')"';
+		echo 'var text = "<tt>passcode accepted<br /><br />Tracing route<br />...<br />...<br />...<br />...<br />...<br />[127.215.1927.1952] holonet_terminal{rra}_kuat<br />[17123.1241.73245.1235] subspace_router{laa}_zeltros<br />[217.1285.123.731587] routing_trml{jha}_toydaria<br />[1581.1925.8341.8123:8322] server_core{KVA}_shadda<br />...<br />...<br />...<br />trace complete<br /><br />holonet::__loadSpike(\'/specialist/spike4.spk\')<br /><br />spikeRout protocol initiated<br />...<br />...<br />...<br />Done<br /><br />holonet::__loadCache(\'/local.trcrt.log\')"';
 	} else {
 		$time = time() + (60 * 30);
 		setcookie('bhg_hashk', md5(time()), $time);
 		echo "var yes = 0\n";
 		echo "var pause = 1\n";
-		echo 'var text = "incorrect passcode<br />...<br />...possible slicing attempt<br />...<br />...spiking signal<br />...<br />...<br />traceback spike ping initiated<br />...<br />...<br />...<br />...<br >...<br />...ident invalid. Locking out resource ID<br />...Terminating<br />...<br />...<br />Connection Terminated<br />"';
+		echo 'var text = "<tt>incorrect passcode<br />...<br />...possible slicing attempt<br />...<br />...spiking signal<br />...<br />...<br />traceback spike ping initiated<br />...<br />...<br />...<br />...<br >...<br />...ident invalid. Locking out resource ID<br />...Terminating<br />...<br />...<br />Connection Terminated<br />"';
 	}
 		
 } elseif (isset($_REQUEST['fiz'])){
 	echo "var id=prompt('Please Enter Administrator Passcode to continue on Hyperspace Route Node DURO', '')\n";
 	echo "var cernun = '&yad=' + id\n";
-	echo 'var text = "ping resolve on id beginning"';
+	echo 'var text = "<tt>ping resolve on id beginning"';
 } elseif (isset($_REQUEST['kad'])){
 	if (in_array($_REQUEST['kad'], array(2292, 2154, 1600, 2687))){
 		echo "var cernun = '&fiz'\n";
-		echo 'var text = "id ping successful. Tracing route<br />...<br />...<br />...<br />...<br />...<br />[122.1239.1235.123] holonet_terminal{kasa}_solrahl<br />[127.0.0.1] routing_trml{asa}_pltalon<br />[12841.1231.21418.212] subspace_router{jha}_firrerre<br />[182.2125.231.1231] subspace_router{jga}_moorja<br />[12312.734.862.258] holonet_terminal{kkd}_duro<br /><br />...trace pause...<br /><br />...<br />...<br />system requires validation to continue traceroute<br />...<br />"';
+		echo 'var text = "<tt>id ping successful. Tracing route<br />...<br />...<br />...<br />...<br />...<br />[122.1239.1235.123] holonet_terminal{kasa}_solrahl<br />[127.0.0.1] routing_trml{asa}_pltalon<br />[12841.1231.21418.212] subspace_router{jha}_firrerre<br />[182.2125.231.1231] subspace_router{jga}_moorja<br />[12312.734.862.258] holonet_terminal{kkd}_duro<br /><br />...trace pause...<br /><br />...<br />...<br />system requires validation to continue traceroute<br />...<br />"';
 		
 	} else {
 		echo "var yes = 0\n";
-		echo 'var text = "cannot resolve ping...<br />...<br />...<br /><br />Terminating<br />...<br />...<br />Connection Terminated<br />"';
+		echo 'var text = "<tt>cannot resolve ping...<br />...<br />...<br /><br />Terminating<br />...<br />...<br />Connection Terminated<br />"';
 	}
 } elseif (isset($_REQUEST['pxr'])){
 	echo "var id=prompt('Please Enter Local Starting Point for Trace (xxxx)', '')\n";
 	echo "var cernun = '&kad=' + id\n";
-	echo 'var text = "ping resolve on id beginning"';
+	echo 'var text = "<tt>ping resolve on id beginning"';
 } else {
 	if (empty($_SERVER['PHP_AUTH_USER']) || strlen($_SERVER['PHP_AUTH_USER']) == 0)
 		reject();
@@ -80,7 +80,7 @@ if (isset($_REQUEST['hhcbqa'])){
 			reject();
 	}
 	echo "var cernun = '&pxr'\n";
-	echo 'var text = "entering passcode:<br />****-****-****<br />validating<br />...<br />...<br />...<br />approved<br /><br />Successfully logged in to RubicoNet Server 129K9 [LYARNA-GUILD]<br /><br />...<br />..."';
+	echo 'var text = "<tt>entering passcode:<br />****-****-****<br />validating<br />...<br />...<br />...<br />approved<br /><br />Successfully logged in to RubicoNet Server 129K9 [LYARNA-GUILD]<br /><br />...<br />..."';
 	
 }
 
