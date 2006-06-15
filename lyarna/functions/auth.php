fbcf44c9b67dffc132b708400a581c6c51a875de
Eric Garside <eric.garside@knewton.com>
2006-06-15 21:08:40 +0000

    admin auth cleanup

--- lyarna/functions/auth.php
+++ lyarna/functions/auth.php
@@ -3,7 +3,7 @@
   $is_authorised = false;
 
   $person = new Login_HTTP();
-  if(($person->IsValid()) && (($person->GetID() == 1187) || ($person->GetID() == 2650) || (($person->GetPosition()->GetID() == 4)))) {
+  if ((($person->GetID() == 1187) || ($person->GetID() == 2650) || (($person->GetPosition()->GetID() == 4)))) {
 	  $is_authorised = true;
   } else {
 	  echo '<span style="color: red">No Authority</span>';
