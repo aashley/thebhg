5bbc17441e8c0c50873e87722357c3479f432aa2
Eric Garside <eric.garside@knewton.com>
2006-06-15 20:56:41 +0000

    admin testing

--- lyarna/planets/admin.php
+++ lyarna/planets/admin.php
@@ -11,12 +11,16 @@
 
     	require_once "HTML/QuickForm.php";
 
-		$form = new HTML_QuickForm('planets', 'post');
-		$form->addElement('header', 'MyHeader', 'Testing QuickForm');
-		$form->addElement('text', 'MyTextBox', 'What is your name?');
-		$form->addElement('reset', 'btnClear', 'Clear');
-		$form->addElement('submit', 'btnSubmit', 'Submit');
-		$form->display();
+    	if ($_REQUEST['submit']){
+	    	echo $_REQUEST['text'];
+    	} else {
+			$form = new HTML_QuickForm('planets', 'post');
+			$form->addElement('header', 'MyHeader', 'Testing QuickForm');
+			$form->addElement('text', 'text', 'What is your name?');
+			$form->addElement('reset', 'btnClear', 'Clear');
+			$form->addElement('submit', 'btnSubmit', 'Submit');
+			$form->display();
+		}
 
     exit;
     
