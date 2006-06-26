b827d4e09fe12dc9fa9bf8739ac4bb58d8424370
Eric Garside <eric.garside@knewton.com>
2006-06-26 19:45:47 +0000

    debugging

--- tactician/cg/administration/edit_type.php
+++ tactician/cg/administration/edit_type.php
@@ -7,21 +7,6 @@ if ($level == 3) {
 	
 	if ($_REQUEST['delete']){
 		
-		if (isset($_REQUEST['event'])){
-			
-			$event = new CGType($_REQUEST['event'], $db);
-			
-			if ($event->DeleteHandler($_REQUEST['del_flag'])){
-				echo 'Event successfully '.strtolower($_REQUEST['del_flag']).'d.';
-			} else {
-				echo "Error from Delete Handler";
-			}
-			
-		} else {
-			echo 'Error passing the required event field';
-		}
-		
-	
 		
 	} else {
 	
