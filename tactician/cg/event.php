9998a9c0dedf0c66021517e87e542178cd3504db
Eric Garside <eric.garside@knewton.com>
2006-06-26 22:00:32 +0000

    team event - support

--- tactician/cg/event.php
+++ tactician/cg/event.php
@@ -8,7 +8,9 @@ add_menu(array('CG ' . roman($cg->GetID())=>'cg/cg.php?id=' . $cg->GetID()));
 
 $table = new Table('', true);
 $table->StartRow();
-$table->AddHeader('Name');
+if (!$event->isTeam()){
+	$table->AddHeader('Name');
+}
 $table->AddHeader('Cadre');
 $table->AddHeader('Points');
 $table->AddHeader('Credits');
@@ -22,7 +24,9 @@ if ($signups) {
 		$cadre =& $signup->GetCadre();
 
 		$table->StartRow();
-		$table->AddCell('<a href="hunter.php?cg=' . $cg->GetID() . '&amp;id=' . $person->GetID() . '">' . $person->GetName() . '</a>');
+		if (!$event->isTeam()){
+			$table->AddCell('<a href="hunter.php?cg=' . $cg->GetID() . '&amp;id=' . $person->GetID() . '">' . $person->GetName() . '</a>');
+		}
 		$table->AddCell('<a href="cadre.php?cg=' . $cg->GetID() . '&amp;cadre=' . $cadre->GetID() . '">' . $cadre->GetName() . '</a>');
 		$table->AddCell('<div style="text-align: right">' . number_format($signup->GetPoints()) . '</div>');
 		$table->AddCell('<div style="text-align: right">' . number_format($signup->GetCredits()) . '</div>');
