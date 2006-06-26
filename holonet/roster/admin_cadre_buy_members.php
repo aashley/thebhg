50285e82aebb9eca4a7496094d3eebaa8afa0b73
Eric Garside <eric.garside@knewton.com>
2006-06-26 04:56:46 +0000

    cadre restructure

--- holonet/roster/admin_cadre_buy_members.php
+++ holonet/roster/admin_cadre_buy_members.php
@@ -52,7 +52,7 @@ function output() {
 	      elseif ($m->getRank->getWeight() == $meet && !$hasEqual)
 	      	$canJoin = true;
 	      
-	      if ($canJoin){
+	      if ($canJoin && !$m->InCadre()){
 		      if ($m->setCadre($cadre->getID())) {
 		
 		        print $m->GetName().' added to Cadre roster.<br><br>';
@@ -173,7 +173,7 @@ function output() {
             .$m->GetName().'</a>',
           ($m->InCadre() ? 'Yes' : 'No'),
           ($temp ? 'Yes' : 'No'),
-          ($temp 
+          (($temp && !$m->InCadre())
            ? '<a href="'.internal_link('admin_cadre_buy_members',
                                        array('submit'=>'add',
                                              'hunter'=>$m->GetID()))
