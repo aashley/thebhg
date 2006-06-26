825d0fcc674fbc72c73fc70d67b310e85094d9cd
Eric Garside <eric.garside@knewton.com>
2006-06-26 20:21:04 +0000

    debugging

--- tactician/cg/administration/submit.php
+++ tactician/cg/administration/submit.php
@@ -5,7 +5,25 @@ page_header('Submit For Event');
 
 if ($level <= 3) {
 	
-	
+	$events = array();
+
+		if (is_array($ka->GetOpenCGs())){
+			foreach ($ka->GetOpenCGs() as $CG){
+				if (is_array($CG->GetHunterSignups($user->GetID()))){
+					foreach ($CG->GetHunterSignups($user->GetID()) as $signup){
+						if (!$signup->GetSubmitted()){
+							$cadre = $signup->GetCadre();
+							$event = $signup->GetEvent();
+							if ($event->IsTimed()){
+								if ($event->GetStart() < time() && $event->GetEnd() > time()){
+									$events[] = $event;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 }
 else {
 	echo 'You are not authorised to access this page.';
