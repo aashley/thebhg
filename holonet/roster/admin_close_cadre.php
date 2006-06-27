c5fd4a05b37fc9ed5d8410d3d169c580e54d44c8
Eric Garside <eric.garside@knewton.com>
2006-06-27 02:18:06 +0000

    adding cadre close function for underlord

--- holonet/roster/admin_close_cadre.php
+++ holonet/roster/admin_close_cadre.php
@@ -30,48 +30,13 @@ function output() {
 	else {
 		$form = new Form($page);
 		$form->StartSelect('Cadre:', 'id');
-		foreach ($roster->GetCadres('name') as $div) {
+		foreach ($roster->GetCadres() as $div) {
 			$form->AddOption($div->GetID(), $div->GetName());
 		}
 		$form->EndSelect();
 		$form->AddSubmitButton('submit', 'Delete Cadre');
 		$form->EndForm();
 	}
-  
-  
-  
-  if (   isset($_REQUEST['confirm'])
-      && $_REQUEST['confirm'] == 'yes') {
-    
-    if ($cadre->Close()) {
-
-      echo 'Cadre has been closed.';
-
-      $pleb = $roster->GetPerson($pleb->GetID());
-
-      // Update the menu
-      $auth_data = get_auth_data($pleb);
-
-    } else {
-
-      echo 'Could not close Cadre: '.$cadre->Error();
-
-    }
-    
-  } else {
-
-    echo 'Are you really sure you wish to close the \''.$cadre->GetName()
-      .'\' cadre?<br><br>'
-      .'Once closed it can not be restored and will need to be recreated.'
-      .'<br><br>'
-      .'[ '
-      .'<a href="'.internal_link('admin_cadre_close', array('confirm'=>'yes'))
-      .'">Yes</a> '
-      .'| '
-      .'<a href="'.internal_link('admin', array()).'">No</a> '
-      .']<br>';
-
-  }
 
   admin_footer($auth_data);
 
