cda45d8a91a002127d20e02dab18f60944a709a1
Adam Ashley <aashley@adamashley.name>
2006-08-18 02:06:33 +0000

    Donate site

---
+++ donate/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+	"http://www.w3.org/TR/xhtml11/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en_AU.UTF-8">
+<head>
+	<title>Donate to help support the BHG Server</title>
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8"/>
+	<meta http-equiv="content-style-type" content="text/css"/>
+	<link rel="alternate" type="application/rss+xml" title="Optimiser Digital Management Zone News Feed" href="/news/rss" />
+	<link rel="stylesheet" href="undohtml.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+</head>
+<body>
+	<h1>Donate to the BHG Server Fund</h1>
+	<h2>(Also known as the Buy Koral A New Bike Fund)</h2>
+	<div class="donate">
+		<h2>Paypal</h2>
+		<p>Those with paypal accounts. I can not accept direct CCs through Paypal. They want
+		money per month for that.</p>
+	</div>
+	<div class="donate">
+		<h2>Paymate</h2>
+		<p>Use this if you just want to donate with a CC without setting up a Paypal account.</p>
+	</div>
+	<p>Keeping the server up and running is an expensive and time consuming proposition. Until
+	2004 I had been funding it all myself, at the time it was costing me US$109, approximately
+	10% of my after tax take home pay. I could not keep this up, so started looking for donations.
+	Now the server is cheaper US$69 a month and considerably more powerful.</p>
+	<p>So feel free to donate as little or as much as you want. And remember donate and you
+	get the contributors access to the <a href="http://www.lyarna.net/">free hosting</a>.</p>
+</body>
+</html>
