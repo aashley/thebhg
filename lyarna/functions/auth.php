825bf3b5dbfd256e305c4687140db3d9e9233bc7
Ian Perry <nightweaver@ianperry.name>
2006-09-23 17:33:20 +0000

    Added MARL and myself to admins.

--- lyarna/functions/auth.php
+++ lyarna/functions/auth.php
@@ -2,8 +2,11 @@
   include_once ('roster.inc');
   $is_authorised = false;
 
+  $valid_ids = array(484, 1187, 2650); // NW, Slag, Grav.
+  $valid_positions = array(4, 7); // Specialist, Marshal.
+
   $person = new Login_HTTP();
-  if ((($person->GetID() == 1187) || ($person->GetID() == 2650) || (($person->GetPosition()->GetID() == 4)))) {
+  if (in_array($person->GetID(), $valid_ids) || in_array($person->GetPosition()->GetID(), $valid_positions)) {
 	  $is_authorised = true;
   } else {
 	  echo '<span style="color: red">No Authority</span>';
