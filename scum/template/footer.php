9e3ce79b87afb46e092e52b6948c8e28df8da96a
Adam Ashley <aashley@adamashley.name>
2006-12-18 03:52:50 +0000

    Update banner display code

--- scum/template/footer.php
+++ scum/template/footer.php
@@ -1,25 +1,22 @@
 </td></tr></table></td></tr>
 <tr><td id="bottom" colspan="2"></td></tr>
-<tr><td colspan="2" id="google" style="text-align: right"><script language='JavaScript' type='text/javascript'>
-<!--
-// Insert click tracking URL here
-document.phpAds_ct0 ='Insert_Clicktrack_URL_Here'
-
-var awrz_rnd = Math.floor(Math.random()*99999999999);
-var awrz_protocol = location.protocol.indexOf('https')>-1?'https:':'http:';
-if (!document.phpAds_used) document.phpAds_used = ',';
-document.write ("<" + "script language='JavaScript' type='text/javascript' src='");
-document.write (awrz_protocol+"//banner.thebhg.org/adjs.php?n=a6ae4dab");
-document.write ("&zoneid=3");
-document.write ("&exclude=" + document.phpAds_used);
-document.write ("&loc=" + escape(window.location));
-if (document.referrer)
-  document.write ("&referer=" + escape(document.referrer));
-document.write ('&r=' + awrz_rnd);
-document.write ("&ct0=" + escape(document.phpAds_ct0));
-document.write ("'><" + "/script>");
-//-->
-</script><noscript><a href='http://banner.thebhg.org/adclick.php?n=a6ae4dab' target='_blank'><img src='http://banner.thebhg.org/adview.php?zoneid=3&n=a6ae4dab' border='0' alt=''></a></noscript></td></tr></table>
+<tr><td colspan="2" id="google" style="text-align: right">
+<!--/* Max Media Manager Javascript Tag v0.3.30-alpha */-->
+<script type='text/javascript'><!--//<![CDATA[
+var m3_u = (location.protocol=='https:'?'https://banner.thebhg.org/www/delivery/ajs.php':'http://banner.thebhg.org/www/delivery/ajs.php');
+var m3_r = Math.floor(Math.random()*99999999999);
+if (!document.MAX_used) document.MAX_used = ',';
+document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+document.write ("?zoneid=3");
+document.write ('&amp;cb=' + m3_r);
+if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+document.write ("&amp;loc=" + escape(window.location));
+if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+if (document.context) document.write ("&context=" + escape(document.context));
+if (document.context) document.write ("&context=" + escape(document.context));
+if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+document.write ("'></scr"+"ipt>");
+//]]>--></script><noscript><a href='http://banner.thebhg.org/www/delivery/ck.php?n=a169bc55&amp;cb=48963257' target='_blank'><img src='http://banner.thebhg.org/www/delivery/avw.php?zoneid=3&amp;n=a169bc55' border='0' alt='' /></a></noscript>
 
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
