571fb38519000054660d7fad05fd9380704cf70f
Adam Ashley <aashley@adamashley.name>
2006-12-19 04:46:45 +0000

    Update banner code

--- holonet/index.php
+++ holonet/index.php
@@ -132,23 +132,23 @@ echo implode(', ', $cnames) . '.<br>Bugs should be reported at the <a href="http
 		<script language='JavaScript' type='text/javascript'>
 		<!--
 		// Insert click tracking URL here
-    document.phpAds_ct0 ='Insert_Clicktrack_URL_Here'
+		document.phpAds_ct0 ='Insert_Clicktrack_URL_Here'
 
-    var awrz_rnd = Math.floor(Math.random()*99999999999);
-    var awrz_protocol = location.protocol.indexOf('https')>-1?'https:':'http:';
-	  if (!document.phpAds_used) document.phpAds_used = ',';
+		var awrz_rnd = Math.floor(Math.random()*99999999999);
+		var awrz_protocol = location.protocol.indexOf('https')>-1?'https:':'http:';
+		if (!document.phpAds_used) document.phpAds_used = ',';
 		document.write ("<" + "script language='JavaScript' type='text/javascript' src='");
-		document.write (awrz_protocol+"//banner.thebhg.org/adjs.php?n=a224b562");
-		document.write ("&zoneid=20");
+		document.write (awrz_protocol+"//banner.thebhg.org/adjs.php?n=a6ae4dab");
+		document.write ("&zoneid=3");
 		document.write ("&exclude=" + document.phpAds_used);
 		document.write ("&loc=" + escape(window.location));
 		if (document.referrer)
-	    document.write ("&referer=" + escape(document.referrer));
+		  document.write ("&referer=" + escape(document.referrer));
 		document.write ('&r=' + awrz_rnd);
 		document.write ("&ct0=" + escape(document.phpAds_ct0));
 		document.write ("'><" + "/script>");
 		//-->
-		</script><noscript><a href='http://banner.thebhg.org/adclick.php?n=a224b562' target='_blank'><img src='http://banner.thebhg.org/adview.php?zoneid=20&n=a224b562' border='0' alt=''></a></noscript>
+		</script><noscript><a href='http://banner.thebhg.org/adclick.php?n=a6ae4dab' target='_blank'><img src='http://banner.thebhg.org/adview.php?zoneid=3&n=a6ae4dab' border='0' alt=''></a></noscript></td></tr></table>
 		</div>
 	</div>
 </body>
