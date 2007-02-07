c9900bf2aa7129dd733a5739f355d2a1b4df8b18
Adam Ashley <aashley@adamashley.name>
2007-02-07 01:56:22 +0000

    Remove bounties from tactician site

--- tactician/bounties/index.php
+++ tactician/bounties/index.php
@@ -1,19 +1,3 @@
 <?
-require_once '../Layout.inc';
-require_once 'HTML/Table.php';
-$site = $_GET['site'];
-$subarray = array(
-                    'View Bounties' => 'bounties/index.php?site=index',
-                    'View All Old Bounties' => 'bounties/index.php?site=old_bounties',
-                    'Administration' => 'bounties/admin/index.php',
-                    'Log out' => 'bounties/index.php?site=logout');
-if($site == "")
-{
-    $site = "index";
-include ("index.inc");
-}else{
-    Include("" . $site . ".inc");
-}
-
-ConstructLayout();
+header("Location: http://ka.thebhg.org/bounties/");
 ?>
