c09df8eff0b1a35157550763208efdd266535d31
Adam Ashley <aashley@adamashley.name>
2007-02-07 01:05:52 +0000

    include the loading of the ka objects correctly

--- ka/bounties/index.php
+++ ka/bounties/index.php
@@ -1,18 +1,22 @@
 <?
+ini_set('include_path', './objects:'.ini_get('include_path'));
+require_once 'ka.inc';
 require_once '../Layout.inc';
 require_once 'HTML/Table.php';
-$site = $_GET['site'];
+
+$ka = new KA('kabals-4ever');
+
 $subarray = array(
                     'View Bounties' => 'bounties/index.php?site=index',
                     'View All Old Bounties' => 'bounties/index.php?site=old_bounties',
                     'Administration' => 'bounties/admin/index.php',
                     'Log out' => 'bounties/index.php?site=logout');
-if($site == "")
-{
-    $site = "index";
-include ("index.inc");
-}else{
-    Include("" . $site . ".inc");
+if($_GET['site'] == "") {
+  $site = "index";
+	include 'index.inc';
+} else {
+	$site = $_GET['site'];
+  include $site . '.inc';
 }
 
 ConstructLayout();
