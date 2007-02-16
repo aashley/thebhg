4667cbd5037f6da484f6c9d366f71681b953e383
Adam Ashley <aashley@adamashley.name>
2007-02-16 05:43:37 +0000

    Correct typo in table name

--- roster4/src/news/item.php
+++ roster4/src/news/item.php
@@ -28,7 +28,7 @@ class bhg_news_item extends bhg_core_base {
 	 * @return void
 	 */
 	public function __construct($id) {
-		parent::__construct('bhg_news_item', $id);
+		parent::__construct('news_item', $id);
 		$this->__addFieldMap(array(
 					'section' => 'bhg_core_code',
 					'poster' => 'bhg_roster_person',
