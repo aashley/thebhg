a7c604d8fc712f9ca9ce42ffe628917235e2ca7c
Adam Ashley <aashley@adamashley.name>
2007-02-16 01:09:36 +0000

    Correct mapping of target field on bhg_roster_pending_transfer

--- roster4/src/roster/pending/transfer.php
+++ roster4/src/roster/pending/transfer.php
@@ -31,7 +31,7 @@ class bhg_roster_pending_transfer extends bhg_core_base {
 		parent::__construct('roster_pending_transfer', $id);
 		$this->__addFieldMap(array(
 					'person' => 'bhg_roster_person',
-					'target' => 'bhg_roster_person',
+					'target' => 'bhg_roster_division',
 					));
 		$this->__addDefaultCodePermissions('set', 'god');
 	}
