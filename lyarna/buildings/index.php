b58212b894437af878f0339070870b1bc02be2de
Eric Garside <eric.garside@knewton.com>
2007-08-29 12:27:50 +0000

    Fixing value display error

--- lyarna/buildings/index.php
+++ lyarna/buildings/index.php
@@ -63,7 +63,7 @@ if (isset($_REQUEST['type'])) {
       $layout[$i] = str_replace("%LOCATION%", $location, $layout[$i]);
       $layout[$i] = str_replace("%OWNER%", $building_info['owner'], $layout[$i]);
       $layout[$i] = str_replace("%TYPE%", $building_info['type'], $layout[$i]);
-      $layout[$i] = str_replace("%VALUE%", ($building_info['value'] ? number_format($building_info['value']) . 'ICs' : 'Pending Evaluation'), $layout[$i]);
+      $layout[$i] = str_replace("%VALUE%", ($building_info['value'] ? number_format($building_info['value']) . ' ICs' : 'Pending Evaluation'), $layout[$i]);
       $arena = ($building_info['arena'] == 1) ? "can" : "can't";
       $layout[$i] = str_replace("%ARENA%", $arena, $layout[$i]);
       $layout[$i] = str_replace("%DESC%", nl2br($building_info['misc']), $layout[$i]);
