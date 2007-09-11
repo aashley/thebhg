94eec680dfa5af08243ff1652f0638d68bc36095
Adam Ashley <aashley@adamashley.name>
2007-09-11 01:51:44 +0000

    Update DB details for new server

--- mybhg/db.php
+++ mybhg/db.php
@@ -1,4 +1,4 @@
 <?php
-$db = mysql_connect('localhost', 'thebhg', 'monkey69');
-mysql_select_db('thebhg_mybhg', $db);
+$db = mysql_connect('localhost', 'thebhg', '1IHfHTsAmILMwpP');
+mysql_select_db('mybhg', $db);
 ?>
